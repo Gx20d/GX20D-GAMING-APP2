072def51a055ef97181794c1173b27d2b3772462
Gx20d <Gx20del@gmail.com>
2026-07-17 15:46:16 +0300

    Create GamesScreen widget for games page

---
+++ lib/screens/games.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class GamesScreen extends StatelessWidget {
+  const GamesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Games"),
+      ),
+      body: const Center(
+        child: Text(
+          "Games Page 🎮",
+          style: TextStyle(fontSize: 22),
+        ),
+      ),
+    );
+  }
+}
