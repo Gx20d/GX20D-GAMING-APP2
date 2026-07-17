13b80686f410205fca75f6f6e627e2454727465b
Gx20d <Gx20del@gmail.com>
2026-07-17 15:47:27 +0300

    Create AppsScreen widget in apps.dart
    Added AppsScreen widget with a simple layout.

---
+++ lib/screens/apps.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class AppsScreen extends StatelessWidget {
+  const AppsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Apps"),
+      ),
+      body: const Center(
+        child: Text(
+          "Apps Page 📱",
+          style: TextStyle(
+            fontSize: 22,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
+  }
+}
